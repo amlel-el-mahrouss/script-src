572e51ed575536c84fec7ff77892218011f594fb
Amlal <amlalelmahrouss@icloud.com>
2023-03-26 10:40:50 +0200

    ScriptNgine: Add ScriptEngine.cs
    Signed-off-by: Amlal <amlalelmahrouss@icloud.com>

---
+++ ScriptNgine/ScriptNgine/ScriptEngine.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ScriptNgine
+{
+    public class ScriptNgineManager
+    {
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern public void RegisterClass(string namespase, string klass);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        static extern public void RegisterEvent(string klass);
+    }
+}
